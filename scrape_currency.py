490fda776e723e8156cbac8adbd7ad6b537cc9fc
Maaz <maaz.sayyid@gmail.com>
2022-10-03 21:34:37 +0530

    Create scrape_currency.py

---
+++ scrape_currency.py
@@ -0,0 +1,17 @@
+# Author: Ardit Sulce, Automate Everything with Python, Udemy
+# Course URL: https://www.udemy.com/course/automate-everything-with-python/
+
+from bs4 import BeautifulSoup
+import requests
+
+def get_currency(in_currency, out_currency):
+  url = f'https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1'
+  content = requests.get(url).text
+  soup = BeautifulSoup(content, 'html.parser')
+  rate = soup.find("span", class_="ccOutputRslt").get_text()
+  rate = float(rate[:-4])
+  
+  return rate
+
+current_rate = get_currency('EUR', 'AUD')
+print(current_rate)
