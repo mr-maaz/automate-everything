f0d8275c78f52b575082c4ef45ead731fc31c165
Maaz <maaz.sayyid@gmail.com>
2022-10-03 21:39:31 +0530

    Create download_file.py

---
+++ download_file.py
@@ -0,0 +1,12 @@
+# Author: Ardit Sulce, Automate Everything with Python, Udemy
+# Course URL: https://www.udemy.com/course/automate-everything-with-python/
+
+url = "https://filesamples.com/samples/audio/mp3/Symphony%20No.6%20(1st%20movement).mp3"
+
+import requests
+
+req = requests.get(url)
+content = req.content
+
+with open('dowload.mp3', 'wb') as file:
+  file.write(content)
