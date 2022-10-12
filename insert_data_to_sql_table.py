424f8f92554ca9fa3b5cd766b3a568b2603e4cf1
Maaz <maaz.sayyid@gmail.com>
2022-10-12 16:46:45 +0530

    Create insert_data_to_sql_table.py

---
+++ insert_data_to_sql_table.py
@@ -0,0 +1,18 @@
+# Author: Ardit Sulce, Automate Everything with Python, Udemy
+# Course URL: https://www.udemy.com/course/automate-everything-with-python/
+
+import sqlite3
+
+con = sqlite3.connect('database.db')
+cur = con.cursor()
+
+new_rows = [
+  ('100.100.100.100', 'a.b.c', 100),
+  ('200.200.200.200', 'f.d.e', 200)
+]
+
+cur.executemany("INSERT INTO 'ips' VALUES(?,?,?)", new_rows)
+con.commit()
+
+cur.execute("SELECT * FROM 'ips'")
+print(cur.fetchall())
