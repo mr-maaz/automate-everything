389865370fc1d69453cd04df85b393db6804136c
Maaz <maaz.sayyid@gmail.com>
2022-10-12 11:53:22 +0530

    Update currency_exchange_rate.py

--- currency_exchange_rate.py
+++ currency_exchange_rate.py
@@ -1,3 +1,6 @@
+# Author: Ardit Sulce, Automate Everything with Python, Udemy
+# Course URL: https://www.udemy.com/course/automate-everything-with-python/
+
 from forex_python.converter import CurrencyRates
 import datetime
 
