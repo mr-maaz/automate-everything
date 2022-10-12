95d7b9f4289f7edcb631c85f79ff3ff2b2a90f1c
Maaz <maaz.sayyid@gmail.com>
2022-10-12 16:34:52 +0530

    Create read_text_files.py

---
+++ read_text_files.py
@@ -0,0 +1,7 @@
+# Author: Ardit Sulce, Automate Everything with Python, Udemy
+# Course URL: https://www.udemy.com/course/automate-everything-with-python/
+
+with open('file1.txt', 'r') as file:
+  content = file.read()
+
+print(content)
