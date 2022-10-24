0a739d3a7b7d5cabf50dbacf9eb221c2f8b1669e
Maaz <maaz.sayyid@gmail.com>
2022-10-24 17:40:14 +0530

    Create get_column_and_row.py

---
+++ get_column_and_row.py
@@ -0,0 +1,24 @@
+# Author: Ardit Sulce, Automate Everything with Python, Udemy
+# Course URL: https://www.udemy.com/course/automate-everything-with-python/
+
+import gspread
+
+# Establish connection
+gc = gspread.service_account('secrets.json')
+
+# Get spreadsheet
+spreadsheet = gc.open('weather_private') 
+
+# Get worksheet
+worksheet1 = spreadsheet.worksheet('2013')
+
+# Get a row or rows by cells
+rows = worksheet1.get_values('A5:F7')
+
+# Get a row by index 
+rows = worksheet1.row_values(3)
+
+# Get a column by index
+column = worksheet1.col_values(2)[1:]
+
+print(rows)
