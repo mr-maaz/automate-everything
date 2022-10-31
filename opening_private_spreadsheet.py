ed2ee5f4f2ecbae7925a620c8609a8ecba18564d
Maaz <maaz.sayyid@gmail.com>
2022-10-31 21:03:18 +0530

    Create opening_private_spreadsheet.py

---
+++ opening_private_spreadsheet.py
@@ -0,0 +1,17 @@
+# Author: Ardit Sulce, Automate Everything with Python, Udemy
+# Course URL: https://www.udemy.com/course/automate-everything-with-python/
+
+import gspread
+
+gc = gspread.service_account('secrets.json')
+
+spreadsheet = gc.open('weather_private')
+
+# Get a worksheet by index
+# worksheet1 = spreadsheet.get_worksheet(0)
+
+# Get a worksheet by name 
+worksheet1 = spreadsheet.worksheet('2013')
+
+data = worksheet1.get_all_records()
+print(data)
