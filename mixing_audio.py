bcdebbce61eba735f5490558fcc54e4709f4561f
Maaz <maaz.sayyid@gmail.com>
2022-11-08 17:27:37 +0530

    Create mixing_audio.py

---
+++ mixing_audio.py
@@ -0,0 +1,18 @@
+# Author: Ardit Sulce, Automate Everything with Python, Udemy
+# Course URL: https://www.udemy.com/course/automate-everything-with-python/
+
+from pydub import AudioSegment
+
+beat = AudioSegment.from_wav('beat.wav')
+sax = AudioSegment.from_wav('sax.wav')
+
+print(len(beat), len(sax))
+
+beat2 = beat * 2
+beat2.export('beat2.wav')
+
+mixed = beat2.overlay(sax)
+mixed.export('mixed.wav')
+
+final = beat2 + mixed * 2 + sax + beat2 + sax
+final.export('final.wav')
